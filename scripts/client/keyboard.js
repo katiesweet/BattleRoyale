a79e609addcd60760261f5bceb1f4af9c4dfa182
Sarah Isaac <sarahisaac31@gmail.com>
2018-04-05 18:51:41 -0600

    base outline for config

--- scripts/client/keyboard.js
+++ scripts/client/keyboard.js
@@ -5,11 +5,23 @@ MyGame.screens['keyboard-config'] = (function(menu, input) {
 		document.getElementById('id-keyboard-back').addEventListener(
 			'click',
 			function() { menu.showScreen('main-menu'); });
+      document.getElementById('fire-config').addEventListener('click', function() { edit('fire') });
+      document.getElementById('rotate-left-config').addEventListener('click', function() { edit('rotate-left') });
+      document.getElementById('rotate-right-config').addEventListener('click', function() { edit('rotate-right') });
+      document.getElementById('move-up-config').addEventListener('click', function() { edit('move-up') });
+      document.getElementById('move-down-config').addEventListener('click', function() { edit('move-down') });
+      document.getElementById('move-right-config').addEventListener('click', function() { edit('move-right') });
+      document.getElementById('move-left-config').addEventListener('click', function() { edit('move-left') });
 	}
 
 	function run() {
 
 	}
+
+  function edit(id) {
+    console.log(id);
+    // open up "Edit tab" and populate the title of what is being edited like move-down command or something
+  }
 
 	return {
 		initialize : initialize,
