bfbede1e647c05c601470e6a5c352aa29eebfaca
Adam <adamjking3@gmail.com>
2018-04-11 13:07:33 -0600

    Add favicon and refactor network.js

--- scripts/client/network.js
+++ scripts/client/network.js
@@ -3,6 +3,28 @@ MyGame.network = (function() {
 
   const socket = io();
   let queue = Queue.create();
+  let listeners = [
+    NetworkIds.CONNECT_ACK,
+    NetworkIds.CONNECT_OTHER,
+    NetworkIds.DISCONNECT_OTHER,
+    NetworkIds.UPDATE_SELF,
+    NetworkIds.UPDATE_OTHER,
+    NetworkIds.BULLET_NEW,
+    NetworkIds.BULLET_HIT,
+  ];
+
+  listeners.forEach(id => {
+    socket.on(id, data => {
+      queue.enqueue({
+        type: id,
+        data: data,
+      });
+    });
+  });
+
+  function emit(...args) {
+    return socket.emit(...args);
+  }
 
   function getQueue() {
     return queue;
@@ -12,65 +34,7 @@ MyGame.network = (function() {
     queue = Queue.create();
   }
 
-  function emit(...args) {
-    return socket.emit(...args);
-  }
-
-  function on(...args) {
-    return socket.on(...args);
-  }
-
-  socket.on(NetworkIds.CONNECT_ACK, data => {
-    queue.enqueue({
-      type: NetworkIds.CONNECT_ACK,
-      data: data,
-    });
-  });
-
-  socket.on(NetworkIds.CONNECT_OTHER, data => {
-    queue.enqueue({
-      type: NetworkIds.CONNECT_OTHER,
-      data: data,
-    });
-  });
-
-  socket.on(NetworkIds.DISCONNECT_OTHER, data => {
-    queue.enqueue({
-      type: NetworkIds.DISCONNECT_OTHER,
-      data: data,
-    });
-  });
-
-  socket.on(NetworkIds.UPDATE_SELF, data => {
-    queue.enqueue({
-      type: NetworkIds.UPDATE_SELF,
-      data: data,
-    });
-  });
-
-  socket.on(NetworkIds.UPDATE_OTHER, data => {
-    queue.enqueue({
-      type: NetworkIds.UPDATE_OTHER,
-      data: data,
-    });
-  });
-
-  socket.on(NetworkIds.BULLET_NEW, data => {
-    queue.enqueue({
-      type: NetworkIds.BULLET_NEW,
-      data: data,
-    });
-  });
-
-  socket.on(NetworkIds.BULLET_HIT, data => {
-    queue.enqueue({
-      type: NetworkIds.BULLET_HIT,
-      data: data,
-    });
-  });
-
   return {
-    on,
     emit,
     getQueue,
     resetQueue,
