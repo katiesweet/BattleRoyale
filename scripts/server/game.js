316a2b637f083931f53d87c6bd86652b6613c09c
Adam <adamjking3@gmail.com>
2018-04-23 17:33:11 -0600

    Make lobby sizing responsive and update chatbox height to height of last message sent.

--- scripts/server/game.js
+++ scripts/server/game.js
@@ -18,8 +18,8 @@ const NetworkIds = require('../shared/network-ids');
 const Queue = require('../shared/queue.js');
 const db = require('./database');
 
-const SIMULATION_UPDATE_RATE_MS = 100;
-const STATE_UPDATE_RATE_MS = 20;
+const SIMULATION_UPDATE_RATE_MS = 100; // 10/sec
+const STATE_UPDATE_RATE_MS = 30; // 3/sec
 const GAME_LENGTH = 8 * 60 * 1000; // 8 min in ms
 let lastUpdate = 0;
 let countdownStarted = false;
@@ -119,14 +119,10 @@ function processInput(elapsedTime) {
           );
           break;
         case NetworkIds.INPUT_ROTATE_LEFT:
-          client.player.rotateLeft(
-            input.message.elapsedTime
-          );
+          client.player.rotateLeft(input.message.elapsedTime);
           break;
         case NetworkIds.INPUT_ROTATE_RIGHT:
-          client.player.rotateRight(
-            input.message.elapsedTime
-          );
+          client.player.rotateRight(input.message.elapsedTime);
           break;
         case NetworkIds.INPUT_FIRE:
           createBullet(input.clientId, client.player);
