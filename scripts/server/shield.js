d954bae8c400f79d663f08b5511ae434c3b9cd96
Sarah Isaac <sarahisaac31@gmail.com>
2018-04-23 11:54:32 -0600

    fix shield radius

--- scripts/server/shield.js
+++ scripts/server/shield.js
@@ -8,7 +8,31 @@ function createShield(spec) {
   let originX = Math.round(random.nextDouble() * 15);
   let originY = Math.round(random.nextDouble() * 15);
 
-  let radius = Math.max(originX - 0, 15 - originX, originY - 0, 15 - originY) + 3; // TODO
+  let points = [
+    {x: 0, y: 0},
+    {x: 0, y: 15},
+    {x: 15, y: 15},
+    {x: 15, y: 0},
+  ];
+
+  let covered = false;
+  let radius = Math.max(originX - 0, 15 - originX, originY - 0, 15 - originY);
+  while(!covered) {
+    covered = true;
+    for (let point in points) {
+      let p = points[point];
+      let xForm = Math.pow(Math.abs(p.x - originX), 2);
+      let yForm = Math.pow(Math.abs(p.y - originY), 2);
+      if (Math.sqrt(xForm + yForm) > radius) {
+        covered = false;
+        break;
+      }
+    }
+    if (!covered) {
+      radius += 1;
+    }
+  }
+
   let rate = radius / 600000; // 600000 is 10 minutes in milliseconds
 
   Object.defineProperty(that, 'originX', {
